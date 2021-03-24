c379b61341b09fc6f588ad5fef6d453e0712aa89
Katherine Gott <katherinegott@Katherines-MacBook-Pro.local>
2021-03-24 16:21:03 -0500

    need to be careful with state in anonymous functions

--- main.go
+++ main.go
@@ -14,12 +14,16 @@ const (
 )
 
 func main() {
-	p2 := powerOfTwo()
-	value := p2()
-	println(value)
-
-	value = p2()
-	println(value)
+	var funcs []func() int64
+	for i := 0; i < 10; i++ {
+		cleanI := i
+		funcs = append(funcs, func() int64 {
+			return int64(math.Pow(float64(cleanI), 2))
+		})
+	}
+	for _, f := range funcs {
+		println(f())
+	}
 }
 
 func powerOfTwo() func() int64 {
