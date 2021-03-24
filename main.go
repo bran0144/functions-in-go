c6a27f00d5a09e580dbfa9cd950a62f01fad6ca1
Katherine Gott <katherinegott@Katherines-MacBook-Pro.local>
2021-03-24 09:23:22 -0500

    using pointer based

--- main.go
+++ main.go
@@ -1,23 +1,36 @@
 package main
 
 import (
-	"fmt"
-	"functions/simplemath"
+	"net/http"
+	"strings"
 )
 
 func main() {
-	answer, err := simplemath.Divide(6, 3)
-	if err != nil {
-		fmt.Printf("An error occurred %s\n", err.Error())
-	} else {
-		fmt.Printf("%f\n", answer)
-	}
-	total := simplemath.Sum(12.2, 14, 16, 22.4)
-	fmt.Printf("total of sum: %f\n", total)
+	// answer, err := simplemath.Divide(6, 3)
+	// if err != nil {
+	// 	fmt.Printf("An error occurred %s\n", err.Error())
+	// } else {
+	// 	fmt.Printf("%f\n", answer)
+	// }
+	// total := simplemath.Sum(12.2, 14, 16, 22.4)
+	// fmt.Printf("total of sum: %f\n", total)
 
-	sv := simplemath.NewSemanticVerion(1, 2, 3)
-	sv.IncrementMajor()
-	sv.IncrementMajor()
-	sv.IncrementMajor()
-	println(sv.String())
+	// sv := simplemath.NewSemanticVerion(1, 2, 3)
+	// sv.IncrementMajor()
+	// sv.IncrementMajor()
+	// sv.IncrementMajor()
+	// println(sv.String())
+
+	var tripper http.RoundTripper = &RoundTripCounter{}
+	r, _ := http.NewRequest(http.MethodGet, "http://pluralsight.com", strings.NewReader("test call"))
+	_, _ = tripper.RoundTrip(r)
+}
+
+type RoundTripCounter struct {
+	count int
+}
+
+func (rt *RoundTripCounter) RoundTrip(*http.Request) (*http.Response, error) {
+	rt.count += 1
+	return nil, nil
 }
